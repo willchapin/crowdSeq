c9353674d3dcdcdafacbca56844f35c0af53df4d
willchapin <wrchapin@gmail.com>
2013-12-03 22:58:31 -0500

    make server

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+var express = require("express")
+  , http = require("http")
+  , socketio = require("socket.io");
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio.listen(server);
+
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/static'));
+});
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    console.log('hello jerks!');
+    console.log(socket.id);
+    socket.emit('fake', 'emitting');
+    socket.broadcast.emit('fake', "broadcasting");
+});
+
+
+
+
+
+
 console.log("hello world!");
